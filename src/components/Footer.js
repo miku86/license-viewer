ee199a64fc79ea676b201b1170d6d382d3feda0f
miku86 <miku86coding@gmail.com>
2018-10-24 19:36:17 +0200

    add header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+  return <footer className="footer">&#169; miku86</footer>;
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>License Viewer</h1>
+    </header>
+  );
+};
+
+export default Header;
