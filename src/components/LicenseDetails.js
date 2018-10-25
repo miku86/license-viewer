d76bb058ef67e2323c1ed75b38d4dd254e082b11
miku86 <miku86coding@gmail.com>
2018-10-25 09:12:53 +0200

    add results

--- src/components/LicenseDetails.js
+++ src/components/LicenseDetails.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 
 class LicenseDetails extends Component {
   render() {
+    const license = this.props.license;
     return (
-      <ul className="result">
-        <li>{this.props.license.title}</li>
-        <li>{this.props.license.description}</li>
-      </ul>
+      <div className="result">
+        {license.title ? (
+          <h1 className="result__title">{license.title}</h1>
+        ) : (
+          ''
+        )}
+        <div className="result__details">
+          <li>
+            {license.description || (
+              <h1 className="select-license">Select a License</h1>
+            )}
+          </li>
+        </div>
+      </div>
     );
   }
 }
