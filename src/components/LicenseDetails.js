8535dbf1c918b09f28cb4b0d6e8cf60ea3ce98d7
miku86 <miku86coding@gmail.com>
2018-11-05 11:52:54 +0100

    remove links from dangerous html

--- src/components/LicenseDetails.js
+++ src/components/LicenseDetails.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import LicenseDetailsBox from './LicenseDetailsBox';
 
 class LicenseDetails extends Component {
+  removeLinks = (text) => text.replace(/(<a href=".+?">|<\/a>)/g, '');
+
   render() {
     const license = this.props.license;
     return license && license.permissions ? (
       <div className="result">
         <h1 className="result__title">{license.title}</h1>
         <div className="result__details">
-          <div className="result__desc">{license.description}</div>
+          <div className="result__desc">
+            {this.removeLinks(license.description)}
+          </div>
           <div className="result__details__boxes">
             <LicenseDetailsBox name="permissions" data={license.permissions} />
             <LicenseDetailsBox name="conditions" data={license.conditions} />
