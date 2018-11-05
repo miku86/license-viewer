7c4ef4e1256961ee23038717a45476bbd2195089
miku86 <miku86coding@gmail.com>
2018-11-05 12:17:16 +0100

    add li key and uppercase

--- src/components/LicenseDetailsBox.js
+++ src/components/LicenseDetailsBox.js
@@ -3,10 +3,10 @@ import React from 'react';
 const LicenseDetailsBox = (props) => {
   return (
     <div className="result__box">
-      <h4 className="result__box__title">{props.name}</h4>
+      <h4 className="result__box__title">{props.name.toUpperCase()}</h4>
       <ul className={`result__${props.name}`}>
         {props.data.map((d) => (
-          <li>{d}</li>
+          <li key={props.name}>{d}</li>
         ))}
       </ul>
     </div>
